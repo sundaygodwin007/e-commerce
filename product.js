ab8aa39ff8b13cf9efda623053e7d8f8168b4cc3
sunday <sundayuduakgodwin@gmail.com>
2023-04-21 13:45:30 -1200

    mobile view of the product page

---
+++ product.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const addToCart = document.querySelector(".add-to-cart");
+
+function add() {
+  document.querySelector("").textContent = "Added To Cart";
+}
+addToCart.addEventListener("click", add);
