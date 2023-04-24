8934558ea88aadfc1dc491ac221ef54bc71ea37a
sunday <sundayuduakgodwin@gmail.com>
2023-04-23 14:10:16 -1200

    updated the carousel for the product page and added a little javascript for the registration page

--- signUp.js
+++ signUp.js
@@ -1,18 +1,24 @@
 const btn = document.querySelector(".btnnn");
 
-// function confirmPassword() {
-//   if (password === password2) {
-//     alert(`welcome! Hope you have a good time shopping`);
-//   } else {
-//     alert(`sorry password and confirm password dont match`);
+// function confirm() {
+//   const pw = document.querySelector("#pswd").value;
+
+//   if (pw === "") {
+//     alert(`passwords needed`);
 //   }
 // }
-// btn.addEventListener("click", confirmPassword);
+// btn.addEventListener("click", confirm);
 
-btn.addEventListener("click", function () {
-  if (password === password2) {
-    alert(`welcome! Hope you have a good time shopping`);
-  } else {
-    alert(`sorry password and confirm password dont match`);
+function verifyPassword() {
+  const pw = document.querySelector("#pswd").value;
+  const pw2 = document.querySelector("#pswd2").value;
+
+  if (pw === "") {
+    document.querySelector("#message").textContent = "Password required";
+  } else if (pw === pw2) {
+    alert(`welcome! hope you have a nice time shopping with us`);
+  } else if (pw !== pw2) {
+    alert(`Sorry password and confirm password don't match`);
   }
-});
+}
+btn.addEventListener("click", verifyPassword);
